a6df836b1cc80296cbdd690ea84a4f245c07a0eb
Alberto Alvarez <albertoalvarez@vinixcode.com>
2023-06-06 11:52:12 -0500

    Rutas post

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,16 @@ Route::post('/wasapi/login', [\App\Http\Controllers\UserController::class, 'auth
 
 
 Route::get('/wasapi/get/none', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
+Route::post('/wasapi/post/none', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
 
 
 Route::group(['middleware' => ['jwt.verify']], function () {
     Route::get('/wasapi/get/token', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
+    Route::post('/wasapi/post/token', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
 });
 
 
 Route::group(['middleware' => ['basic.auth']], function () {
     Route::get('/wasapi/get/user', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
+    Route::post('/wasapi/post/user', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
 });
