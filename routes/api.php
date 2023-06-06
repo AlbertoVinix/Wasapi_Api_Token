cfc3a757f4e0fd2eec0c263920e02e63a6efd388
Alberto Alvarez <albertoalvarez@vinixcode.com>
2023-06-06 06:22:03 -0500

    Token and basic authentication

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/wasapi/login', [\App\Http\Controllers\UserController::class, 'authenticate']);
 
 
+Route::get('/wasapi/get/none', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
+
+
 Route::group(['middleware' => ['jwt.verify']], function () {
-    Route::get('/wasapi/get', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
-    /*AÑADE AQUI LAS RUTAS QUE QUIERAS PROTEGER CON JWT*/
+    Route::get('/wasapi/get/token', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
+});
+
+
+Route::group(['middleware' => ['basic.auth']], function () {
+    Route::get('/wasapi/get/user', [\App\Http\Controllers\WasapiController::class, 'getWasapi']);
 });
